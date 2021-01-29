d74eaf0c2ede45c34200b0fbe2a2c58846726344
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2021-01-30 01:55:31 +0200

    Flight path predictions

--- Assets/Scripts/Physics.cs
+++ Assets/Scripts/Physics.cs
@@ -1,22 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Physics : MonoBehaviour
 {
-    public GameObject Earth;
+    public Vector2 Velocity;
+    public Transform SimulationParticle;
+    private GameObject earth;
     private Rigidbody2D rb;
     private bool started = false;
-    private Vector2 previousVelocity;
     const double GM = 1.2; // Gravity constant * mass of earth
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-3.0f, 3.0f);
-        previousVelocity = rb.velocity;
+        rb.velocity = Velocity;
+        earth = GameObject.Find("Earth");
         started = true;
+    }
+
+    private Vector2 CalculateMovement(Vector3 pos)
+    {
+        double dx = earth.transform.position.x - pos.x;
+        double dy = earth.transform.position.y - pos.y;
+        double r = Math.Sqrt(dx * dx + dy * dy);
+        double acceleration = GM / (r * r);
+        double angle = Math.Atan2(dy, dx);
+        return new Vector2((float)(acceleration * Math.Cos(angle)), (float)(acceleration * Math.Sin(angle)));
     }
 
     // FixedUpdate is called once per frame
@@ -26,13 +39,17 @@ public class Physics : MonoBehaviour
         {
             return;
         }
-        double dx = Earth.transform.position.x - transform.position.x;
-        double dy = Earth.transform.position.y - transform.position.y;
-        double r = Math.Sqrt(dx * dx + dy * dy);
-        double acceleration = GM / (r * r);
-        double angle = Math.Atan2(dy, dx);
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity += new Vector2((float)(acceleration * Math.Cos(angle)), (float)(acceleration * Math.Sin(angle)));
-        previousVelocity = rb.velocity;
+        rb.velocity += CalculateMovement(transform.position);
+        Vector3 simulatedPosition = transform.position;
+        Vector2 simulatedSpeed = rb.velocity;
+        for (int i = 0; i < 100; i++)
+        {
+            simulatedPosition += new Vector3(simulatedSpeed.x, simulatedSpeed.y, 0.0f) * 0.02f;
+            simulatedSpeed += CalculateMovement(simulatedPosition);
+            if (i % 10 == 0)
+            {
+                Instantiate(SimulationParticle, simulatedPosition, Quaternion.identity);
+            }
+        }
     }
 }
