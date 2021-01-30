e4b02bec485386acba6f7e6c82871bde846fc06b
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2021-01-31 01:02:01 +0200

    BOOOM HAHAHA

--- Assets/Scripts/Satellite.cs
+++ Assets/Scripts/Satellite.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,39 +8,57 @@ public class Satellite : MonoBehaviour
     public GameObject[] ShrapnelPrefabs;
     public GameObject ExplosionPrefab;
     public Player player;
+    public AudioSource audioSource_;
+    public List<AudioClip> collisionClips;
+    public List<AudioClip> explosionClips;
 
-    public int armor = 1;
+    public int armor = 3;
     public int comms = 1;
     public float thrust = 1f;
 
+    void Start()
+    {
+        audioSource_ = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (GetInstanceID() > collision.transform.GetInstanceID())
+        armor--;
+        if (armor <= 0)
         {
-            int nr = Random.Range(2, 5) + 10;
-            for (int i = 0; i < nr; i++)
+            if (GetInstanceID() > collision.transform.GetInstanceID())
             {
-                int tr = Random.Range(0, ShrapnelPrefabs.Length);
-                var obj = Instantiate(ShrapnelPrefabs[tr], transform.position, Quaternion.identity);
-                obj.GetComponent<Physics>().Velocity = GetComponent<Physics>().Velocity + Random.insideUnitCircle * 2.0f;
-            }
-
-            Physics other = collision.transform.GetComponent<Physics>();
-            if (other)
-            {
-                nr = Random.Range(2, 5) + 10;
+                int nr = Random.Range(2, 5) + 10;
                 for (int i = 0; i < nr; i++)
                 {
                     int tr = Random.Range(0, ShrapnelPrefabs.Length);
                     var obj = Instantiate(ShrapnelPrefabs[tr], transform.position, Quaternion.identity);
-                    obj.GetComponent<Physics>().Velocity = other.Velocity + Random.insideUnitCircle * 2.0f;
+                    obj.GetComponent<Physics>().Velocity = GetComponent<Physics>().Velocity + Random.insideUnitCircle * 2.0f;
+                }
+
+                Physics other = collision.transform.GetComponent<Physics>();
+                if (other)
+                {
+                    nr = Random.Range(2, 5) + 10;
+                    for (int i = 0; i < nr; i++)
+                    {
+                        int tr = Random.Range(0, ShrapnelPrefabs.Length);
+                        var obj = Instantiate(ShrapnelPrefabs[tr], transform.position, Quaternion.identity);
+                        obj.GetComponent<Physics>().Velocity = other.Velocity + Random.insideUnitCircle * 2.0f;
+                    }
                 }
             }
+            Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+
+            AudioSource.PlayClipAtPoint(explosionClips[Random.Range(0, explosionClips.Count - 1)], this.gameObject.transform.position);
+
+
+            Destroy(gameObject);
+            player.RemoveSatellite(gameObject);
         }
-
-        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
-
-        Destroy(gameObject);
-        player.RemoveSatellite(gameObject);
+        else
+        {
+            AudioSource.PlayClipAtPoint(collisionClips[Random.Range(0, collisionClips.Count - 1)], this.gameObject.transform.position);
+        }
     }
 }
